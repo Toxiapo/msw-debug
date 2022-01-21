368256e9057004c117666c4dc7d93bf3273e68a3
Leon <toxiapo@gmail.com>
2022-01-21 12:23:16 -0500

    use getstaticprops

---
+++ cypress/integration/example.ts
@@ -0,0 +1,22 @@
+//@ts-nocheck
+
+describe('Book detail', () => {
+ 
+  it('handles user authentication', () => {
+		cy.interceptRequest(
+			'GET',
+			'https://jsonplaceholder.cypress.io/comments',
+			(req, res, ctx) => {
+				return res(ctx.json([{
+					body: "Body",
+					email: "Eliseo@gardner.biz",
+					id: 1,
+					name: "John Doe testing",
+					postId: 1,
+				}]))
+			},
+		) 
+    cy.visit('/');
+  })
+})
+export {}
